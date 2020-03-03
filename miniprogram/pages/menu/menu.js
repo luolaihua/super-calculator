623bc4c0f5fd5c6a633a5bbb7619c7f7644bda50
罗三炮 <oncwnuJ0AZUH8t1w6MXPpRjUacvI@git.weixin.qq.com>
2020-03-03 23:13:18 +0800

    翻译机基本完成，字数统计初步

--- miniprogram/pages/menu/menu.js
+++ miniprogram/pages/menu/menu.js
@@ -1,4 +1,34 @@
 // miniprogram/pages/menu/menu.js
+var re = /[\u4E00-\u9FA5]/g; 	
+//g代表可多次匹配正则
+
+var reg = /[\u3002|\uff1f|\uff01|\uff0c|\u3001|\uff1b|\uff1a|\u201c|\u201d|\u2018|\u2019|\uff08|\uff09|\u300a|\u300b|\u3008|\u3009|\u3010|\u3011|\u300e|\u300f|\u300c|\u300d|\ufe43|\ufe44|\u3014|\u3015|\u2026|\u2014|\uff5e|\ufe4f|\uffe5]/g;
+var txt='你好，this is a test? 123456789'
+      var re1=/\d/g;
+      var re2=/[a-zA-Z]/g;
+      var re3=/[\u4e00-\u9fa5]/g;
+      var reg = /[\u3002|\uff1f|\uff01|\uff0c|\u3001|\uff1b|\uff1a|\u201c|\u201d|\u2018|\u2019|\uff08|\uff09|\u300a|\u300b|\u3008|\u3009|\u3010|\u3011|\u300e|\u300f|\u300c|\u300d|\ufe43|\ufe44|\u3014|\u3015|\u2026|\u2014|\uff5e|\ufe4f|\uffe5]/g;
+ var len1=0,len2=0,len3=0,len4=0;
+  if(txt!==""){
+      if(txt.match(re1)!=null){
+          len1=(txt.match(re1)).length; 
+      }
+      if(txt.match(re2)!=null){
+          len2=(txt.match(re2)).length;  
+      }
+      if(txt.match(re3)!=null){
+          len3=(txt.match(re3)).length;
+      }
+      if(txt.match(reg)!=null){
+        len4=(txt.match(re3)).length;
+    }
+  }
+console.log(len1,len2,len3,len4)
+
+
+
+
+
 Page({
 
   /**
@@ -17,7 +47,7 @@ Page({
         url: '../../images/chat.png'
       },
       {
-        name: '北京话生成',
+        name: '翻译机',
         url: '../../images/laugh.png'
       },
       {
