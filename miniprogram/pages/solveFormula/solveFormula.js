59702af17d5e0f9a4dce820c559c532f7177f32e
罗三炮 <oncwnuJ0AZUH8t1w6MXPpRjUacvI@git.weixin.qq.com>
2020-03-23 22:33:19 +0800

    历史记录功能完善，解方程起步

---
+++ miniprogram/pages/solveFormula/solveFormula.js
@@ -0,0 +1,67 @@
+// miniprogram/pages/solveFormula/solveFormula.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    days: ['一', '二', '三', '四', '五'],
+    formulaList:['aX+b=0','aX²+bX+c=0','aX³+bX²+cX+d=0','aX^4+bX³+cX²+dX+e=0',]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
